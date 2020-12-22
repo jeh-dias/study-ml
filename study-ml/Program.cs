1fdc0255a75c092a1ba7a19772b797eadf8847bf
Jéssica Dias <jessicadr@taginfraestrutura.com.br>
2020-12-22 17:40:46 -0300

    Add Test Ml

---
+++ study-ml/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace study_ml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
